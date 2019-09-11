cc25959d4f64ecc6df5a923658cc10dda7a410ef
Doug <me@douglasbowen.com>
2019-09-11 18:04:29 -0400

    finish recipe batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -10,10 +10,39 @@ import math
 
 # Then we need to take the ingredients dictionary and grab the values and put them in their own array
 
-# Then we take the two arrays, put them in a for loop, and maybe we can use a list comprehension, but we can take the two values and divide the recipe by the ingredients with a floor (//) and then take that result and get the minimum from the array, and thats the number of batches that you have. allegedly.
+# Then we take the two arrays, put them in a for loop, and maybe we can use a list comprehension, but we can take the two values and divide the recipe by the ingredients with a floor (recipe/ingredients)(//) and then take that result and get the minimum from the array, and thats the number of batches that you have. allegedly.
+
+recipe = { 'milk': 100, 'butter': 50, 'cheese': 10 }
+ingredients =  { 'milk': 198, 'butter': 52, 'cheese': 10 }
 
 def recipe_batches(recipe, ingredients):
-  pass 
+    print(recipe)
+    print(ingredients)
+    print("")
+    recipeArray = []
+    ingredientsArray = []
+    batches2 = []
+    for k,v in recipe.items():
+        recipeArray.append(v)
+    for k,v in ingredients.items():
+        ingredientsArray.append(v)
+    print(recipeArray)
+    print(ingredientsArray)
+    batches = [(x / y) for x in recipeArray for y in ingredientsArray if x ]
+    print(batches)
+    print(min(batches))
+    if len(ingredientsArray) != len(recipeArray):
+        return 0
+    for x in range(len(recipeArray)):
+       
+        batches2.append(ingredientsArray[x]//recipeArray[x])
+    print(batches2)
+    print(min(batches2))
+   
+  
+    return min(batches2)
+    
+recipe_batches(recipe, ingredients)
 
 
 if __name__ == '__main__':
