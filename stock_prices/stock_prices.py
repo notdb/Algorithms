33a2517509c84e00418749629d220ab6ef081ecd
Doug <me@douglasbowen.com>
2019-09-11 15:38:15 -0400

    add notes to stock prices

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,6 +2,12 @@
 
 import argparse
 
+# Write a function find_max_profit that receives as input a list of stock prices. Your function should return the maximum profit that can be made from a single buy and sell. You must buy first before selling; no shorting is allowed here.
+
+# For example, find_max_profit([1050, 270, 1540, 3800, 2]) should return 3530, which is the maximum profit that can be made from a single buy and then sell of these stock prices.
+
+# 
+
 def find_max_profit(prices):
   pass
 
